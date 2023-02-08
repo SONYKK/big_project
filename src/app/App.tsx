a53386a81d7fb62f77f7f6ce0f5b8e983ad98834
Alexandr Vasenev <alexvasenev@yandex.ru>
2023-02-09 00:31:38 +0300

    FSD-architecture starts

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,11 @@
 import React, {Suspense} from 'react';
 import './styles/index.scss'
 import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
-import {MainPageAsync} from "./pages/MainPage/MainPage.async";
-import {UseTheme} from "./theme/useTheme";
-import {classNames} from "./helpers/classNames/classNames";
+import {UseTheme} from "app/providers/ThemeProvider";
+import { classNames } from 'shared/lib/classNames/classNames';
+import { MainPage } from 'pages/MainPage';
+import { AboutPage } from 'pages/AboutPage';
+
 
 const App = () => {
 
@@ -17,8 +18,8 @@ const App = () => {
             <Link to={'/about'}>About us</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route element={<AboutPageAsync/>} path={'/about'}/>
-                    <Route element={<MainPageAsync/>} path={'/'}/>
+                    <Route element={<AboutPage/>} path={'/about'}/>
+                    <Route element={<MainPage/>} path={'/'}/>
                 </Routes>
             </Suspense>
 
--- webpack.config.ts
+++ webpack.config.ts
@@ -11,6 +11,7 @@ export default (env: BuildEnv) => {
     build: path.resolve(__dirname, 'build'),
     html: path.resolve(__dirname, 'public', 'index.html'),
     entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    src: path.resolve(__dirname, 'src'),
   }
 
   const mode = env.mode || 'development';
